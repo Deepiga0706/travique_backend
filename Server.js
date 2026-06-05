4a7474bcd23da6375ece1f31592a2b9bdbd1e657
Deepiga0706 <deepiga.rs2024cce@sece.ac.in>
2026-06-05 11:50:27 +0530

    backend for travique

--- Server.js
+++ Server.js
@@ -3,19 +3,30 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
 
-const app=express();
+const app = express();
+
 app.use(express.json());
-app.use(cors()); 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000"); 
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Travique Backend Running");
 });
-const UserRoutes=require("./Routers/UserRoutes");
-app.use("/api/user",UserRoutes);
-console.log("Mongo URL:", process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+
+const UserRoutes = require("./Routers/UserRoutes");
+app.use("/api/user", UserRoutes);
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
     console.log("Connected to MongoDB successfully");
-}).catch((err)=>{
+})
+.catch((err) => {
     console.log("Error connecting to MongoDB", err);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
