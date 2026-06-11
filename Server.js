83d8cf2574e3410e31d329bd8ad90777d8a17f26
Deepiga0706 <deepiga.rs2024cce@sece.ac.in>
2026-06-12 00:23:00 +0530

    updated cors

--- Server.js
+++ Server.js
@@ -15,7 +15,13 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Home Route
 app.get("/", (req, res) => {
